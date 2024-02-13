8b1c3e51e67a44a231fa1ee84a19c1218a1dae8f
Afrid Basha Abdul <afridbashaabdul@gmail.com>
2024-02-13 15:56:47 +0530

    Create index.php

---
+++ Kubernetes LEMP Setup/index.php
@@ -0,0 +1,7 @@
+# Update existing php file with this content
+
+<?php
+$dbname = $_ENV["MYSQL_DATABASE"];
+$dbuser = $_ENV["MYSQL_USER"];
+$dbpass = $_ENV["MYSQL_PASSWORD"];
+$dbhost = $_ENV["MYSQL_HOST"];
